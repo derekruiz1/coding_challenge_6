43f5abcbcda27ee36ce08a251ea2e038c527dc29
Derek Ruize <derekruiz@usf.edu>
2025-02-05 22:31:39 -0500

    Task 1 - Business Profit Calculation

---
+++ cc_6.js
@@ -0,0 +1,8 @@
+//Task 1 - Function Declaration
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    [Profit = (sellingPrice - costPrice) * unitsSold] //Formula to calculate profit
+    console.log("Total Profit:", "$" + Profit) 
+};
+calculateProfit(20,30,100); 
+calculateProfit(50,70,200);
+

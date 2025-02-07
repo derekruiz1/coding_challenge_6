f4c2a01a9157b90cf04e86dc70fab4997e238b40
Derek Ruize <derekruiz@usf.edu>
2025-02-07 07:45:08 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -13,3 +13,17 @@ const calculateSalesTax = function calculateSalesTax(amount, taxRate) {
 };
 calculateSalesTax(100,0.07);
 calculateSalesTax(500,0.01);
+
+//Task 3 - Arrow Function
+const calculateBonus = (salary, performanceRating) => {
+    const bonus = {
+        "Excellent": 0.2,
+        "Good": 0.1,
+        "Average": 0.05
+    }; //Parameters for bonus 20%, 10%, 5% respectively
+
+    return console.log("Bonus:" , "$" + salary * (bonus[performanceRating]));
+};
+
+calculateBonus(5000, "Excellent");
+calculateBonus(7000, "Good");
